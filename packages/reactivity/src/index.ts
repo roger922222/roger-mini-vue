125ee9089101f757e6f5bab748144b229d5f0370
roger <m16675184976@163.com>
2022-10-17 10:38:26 +0800

    实现watchEffect

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,3 +1,3 @@
 export { ref, proxyRefs, isRef, unRef } from './ref'
 export { reactive, readonly, shallowReadonly, isReactive, isReadonly, isProxy } from './reactive'
-export { effect } from './effect'
+export { effect, ReactiveEffect } from './effect'
--- packages/runtime-core/src/scheduler.ts
+++ packages/runtime-core/src/scheduler.ts
@@ -1,15 +1,21 @@
 const queue: any[] = []
+const activePreFlushCbs: any[] = []
 let isFlushPending = false
 
 const p = Promise.resolve()
 
-export function nextTick(fn) {
+export function nextTick(fn?) {
   return fn ? p.then(fn) : p
 }
 
 export function queueJobs(job) {
   if (!queue.includes(job)) queue.push(job)
 
+  queueFlush()
+}
+
+export function queuePreFlushCb(job) {
+  activePreFlushCbs.push(job)
   queueFlush()
 }
 
@@ -21,8 +27,19 @@ function queueFlush() {
 
 function flushJobs() {
   isFlushPending = false
+
+  // 组件渲染前
+  flushPreFlusCbs()
+
+  // compoent render
   let job
   while ((job = queue.shift())) {
     job && job()
+  }
+}
+
+function flushPreFlusCbs() {
+  for (let i = 0; i < activePreFlushCbs.length; i++) {
+    activePreFlushCbs[i]()
   }
 }
